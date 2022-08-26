f3e5e39f7d3fa2dc06029b9d55ae2425fd9462f2
petelab <petelab@users.noreply.github.com>
2022-08-26 08:31:03 +0200

    progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,46 +4,66 @@ import styled from "styled-components";
 
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
-
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius:4
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius:¤
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius:8
+  },
+};
 const ProgressBar = ({ value, size }) => {
-  let Component;
-  if (size === "small") {
-    Component = SmallInner;
-  } else if (size === "medium") {
-    Component = MediumInner;
-  } else {
-    Component = StyledInner;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error("unknown size passed to progreessbar");
   }
   return (
-    <StyledDiv pad={size === "medium" ? "5" : null}>
-      <Component value={value} size={size} max="100">
-        {/*  <strong>{value}</strong> */}
-      </Component>
-    </StyledDiv>
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{ "--padding": styles.padding + "px",
+      "--radius": styles.radius + "px" }}
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            "--width": value + "%",
+            "--height": styles.height + "px",
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
   );
 };
 
-const StyledInner = styled.progress`
-  /* width: ${(props) => {
-    return props.value + "%";
-  }}; */
-  width: 98%;
-  margin-left: auto;
-  margin-right: auto;
-  /*background-color: ${COLORS.primary};*/
-  border-radius: ${(props) => (props.value > 99 ? "8px" : null)};
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden; /* trim off corners when progressbar is near full*/
 `;
-const MediumInner = styled(StyledInner)`
-  min-height: 12px;
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  padding: var(--padding);
+  border-radius:var(--radius);
 `;
-const SmallInner = styled(StyledInner)`
-  min-height: 8px;
-`;
-const StyledDiv = styled.div`
-  padding: 0px;
-  /* padding: ${(props) => props.pad + "px"}; */
-  background-color: hsla(0, 0%, 50%, 0.15);
-  border-radius: 8px;
-  width: 370px;
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
 `;
 export default ProgressBar;
